c8473e8540136f0d005104e6efc3524226727d1a
你的用户名 <123@qq.com>
2025-09-15 11:44:52 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // 静态导出配置
+  output: 'export',
+  distDir: 'dist', // 改变输出目录名称
+  trailingSlash: true,
 }
 
 export default nextConfig
